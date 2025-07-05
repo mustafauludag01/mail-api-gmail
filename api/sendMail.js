300a04472c5d11bf7151fee3651a08281b5a7628
mustafauludag01 <mustafa@ozbilim.com>
2025-07-05 20:27:29 +0300

    fix: cors access

--- api/sendMail.js
+++ api/sendMail.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.GMAIL_USER,
